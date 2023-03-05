84d7efa62b4388724d72ea8b4c8396a49efb31f3
Ömer Gönüldaş <124463633+TEQQQQ@users.noreply.github.com>
2023-03-05 17:25:05 +0200

    Create image_fuzifucation.m

---
+++ image_fuzifucation.m
@@ -0,0 +1,15 @@
+i = imread('cameraman.tif');
+f = ones(3,3)/9;
+il = filter2(f,i,'same');
+imshow(il/255);
+figure,imshow(i);
+f1 = fspecial('average',[3,3]);
+i2 = filter2(f1,i,'same');
+figure,imshow(i2/255);
+i2 = filter2(f1,i,'valid');
+figure,imshow(i2/255);
+i2 = filter2(f1,i,'full');
+figure,imshow(i2/255);
+f3 = fspecial('average',11);
+i3 = filter2(f3,i,'same');
+figure,imshow(i3/255);
